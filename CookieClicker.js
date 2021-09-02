5b39e4461af3356d4eca357dc39b53265d689406
Bigfoot <80417201+Bigfoot9999@users.noreply.github.com>
2021-09-02 09:55:23 -0600

    Update CookieClicker.js

--- CookieClicker.js
+++ CookieClicker.js
@@ -2,22 +2,21 @@
 // @name         Cookie Auto-clicker
 // @namespace    https://github.com/Bigfoot9999
 // @version      1.1.0
-// @description  Auto clicker made by bigfoot
+// @description  Auto clicker made by bigfoot (https://raw.githubusercontent.com/Bigfoot9999/Auto-Cookie-Clicker/main/CookieClicker.js)
 // @author       Bigfoot9999
-// @match        https://trixter9994.github.io/Cookie-Clicker-Source-Code/
 // @match        https://*/*
 // @grant        none
-// @updateURL    https://github.com/Bigfoot9999/Auto-Cookie-Clicker/
-// @downloadURL  https://github.com/Bigfoot9999/Auto-Cookie-Clicker/
+// @icon         https://d29fhpw069ctt2.cloudfront.net/icon/image/85131/preview.svg
+// @updateURL    https://raw.githubusercontent.com/Bigfoot9999/Auto-Cookie-Clicker/main/CookieClicker.js
+// @downloadURL  https://raw.githubusercontent.com/Bigfoot9999/Auto-Cookie-Clicker/main/CookieClicker.js
 // ==/UserScript==
 
-//test (successful update)
-
+document.addEventListener("keypress", function(keypress) {
+    alert("Update Chrome OS keyboard for typing functionality");
+});
 (function cookieClick(){
     document.getElementById('bigCookie').click();
     setTimeout(cookieClick, 0);
 })();
 
-document.addEventListener("keypress", function(keypress) {
-        alert("Update Chrome OS keyboard for typing functionality");
-});
+
