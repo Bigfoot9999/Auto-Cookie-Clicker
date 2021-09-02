a525feff9533b2089e9b04f9d5df3ec7a7edd691
Bigfoot <80417201+Bigfoot9999@users.noreply.github.com>
2021-09-02 08:41:07 -0600

    Update CookieClicker.js

--- CookieClicker.js
+++ CookieClicker.js
@@ -5,22 +5,17 @@
 // @description  Auto clicker made by bigfoot
 // @author       Bigfoot9999
 // @match        https://trixter9994.github.io/Cookie-Clicker-Source-Code/
+// @match        https://*/*
 // @grant        none
 // @updateURL    https://github.com/Bigfoot9999/Auto-Cookie-Clicker/
 // @downloadURL  https://github.com/Bigfoot9999/Auto-Cookie-Clicker/
 // ==/UserScript==
-//DOWNLOAD TAMPERMONKEY: https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en
+
 
 (function cookieClick(){
     document.getElementById('bigCookie').click();
     setTimeout(cookieClick, 0);
 })();
-
-
-
-
-
-
 
 document.addEventListener("keypress", function(keypress) {
         alert("Update Chrome OS keyboard for typing functionality");
